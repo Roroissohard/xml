5807be8b60083858cbcba13cbb2e83af594085f0
Romier Manning <126rmanning@frhsd.com>
2026-01-16 08:36:33 -0500

    1/16

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
  // Create XMLHttpRequest Object!
         const xhr = new XMLHttpRequest();
-
+        
         // onload is an EVENT... like a click event
         xhr.onload = function () {
             if(xhr.status === 200){
                 const data = JSON.parse(xhr.responseText);
-            }
+                const players = data.players;
+                const container = document.getElementById("cards");
+                
+                players.forEach(player => {
+                    const card = document.createElement("div");
+                    
+                    let statsHTML = "";
+                    let teamsHTML = "";
+                    let awardsHTML = "";
+                
+                    for(team = 0; team < player.teams.length; team++){
+                    
+                }});
+            {}
+            
         };
+    }
+
+                
+
+                
 
         xhr.open = ("GET", "player.json");
         xhr.send();
+
+        
