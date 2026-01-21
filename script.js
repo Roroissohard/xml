1a765d6cf210e19a2df5a927359688b859bb9d5b
Romier Manning <126rmanning@frhsd.com>
2026-01-21 10:54:07 -0500

    done

--- script.js
+++ script.js
@@ -1,33 +1,78 @@
- // Create XMLHttpRequest Object!
-        const xhr = new XMLHttpRequest();
-        
-        // onload is an EVENT... like a click event
-        xhr.onload = function () {
-            if(xhr.status === 200){
-                const data = JSON.parse(xhr.responseText);
-                const players = data.players;
-                const container = document.getElementById("cards");
-                
-                players.forEach(player => {
-                    const card = document.createElement("div");
-                    
-                    let statsHTML = "";
-                    let teamsHTML = "";
-                    let awardsHTML = "";
-                
-                    for(team = 0; team < player.teams.length; team++){
-                    
-                }});
-            {}
-            
-        };
+const xhr = new XMLHttpRequest();
+
+xhr.onload = function () {
+
+    if (xhr.status === 200) {
+        const data = JSON.parse(xhr.responseText);
+        const players = data.players;
+        const container = document.getElementById("cards");
+
+        players.forEach(player => {
+            const card = document.createElement("div");
+              card.className = "card";
+            //add a className "card" to the card variable
+
+            let statsHTML = "";
+            let teamsHTML = "";
+            let awardsHTML = "";
+
+            // the for loop below is looping through an object 
+            // the syntax is different when you look through arrays
+            for (let stat in player.careerStats) {
+                statsHTML += "<li>" + stat + ": " + player.careerStats[stat] + "</li>";
+            }
+
+            // teams and awards are both arrays, so these for loops should look the same
+            for (let i = 0; i < player.teams.length; i++) {
+                teamsHTML += "<li>" + player.teams[i] + "</li>";
+            }
+
+            for (let i = 0; i < player.awards.length; i++) {
+                awardsHTML += "<li>" + player.awards[i] + "</li>";
+            }
+
+            card.innerHTML = `
+                <h2>
+                ${player.fullName}
+                </h2>
+                <p><span class="label">Position:</span>
+                ${player.position}
+                </p>
+                <p><span class="label">Primary Team:</span> 
+                ${player.primaryTeam}
+                </p>
+                <p><span class="label">Jersey Number:</span> #
+                ${player.jerseyNumber}
+                </p>
+                <p><span class="label">Born:</span>
+                ${player.birthDate}
+                </p>
+                <p><span class="label">Throws:</span>
+                ${player.throws}
+                </p>
+                <p><span class="label">Hall of Fame:</span>
+                ${player.hallOfFameYear}
+                </p>
+
+                <p class="label">Career Stats:</p>
+                <ul>
+                ${statsHTML}
+                </ul>
+
+                <p class="label">Teams:</p>
+                <ul>
+               ${teamsHTML}
+                </ul>
+
+                <p class="label">Awards:</p>
+                <ul>
+               ${awardsHTML}
+                </ul>`;
+
+            container.appendChild(card); //variables go into those spots
+        });
     }
+};
 
-                
-
-                
-
-        xhr.open = ("GET", "player.json");
-        xhr.send();
-
-        
+xhr.open("GET", "player.json"); //make sure your json file is named player.json in VSCode
+xhr.send();
